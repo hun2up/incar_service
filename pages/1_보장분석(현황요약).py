af040c94d41f972f0ac5a150ec33fa90a70e394a
hun2 <hun2up@gmail.com>
2023-11-03 08:27:44 +0900

    service

---
+++ pages/1_보장분석(현황요약).py
@@ -0,0 +1,60 @@
+########################################################################################################################
+##############################################     라이브러리 호출하기     ##################################################
+########################################################################################################################
+import time
+import pandas as pd
+import streamlit as st
+import streamlit_authenticator as stauth
+hashed_passwords = stauth.Hasher(['XXX']).generate()
+import yaml
+from yaml.loader import SafeLoader
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+from utils import hide_st_style ,call_sheets
+from utils import ServiceData
+
+###########################################################################################################################
+################################################     인증페이지 설정     ###################################################
+###########################################################################################################################
+# ---------------------------------------------    페이지 레이아웃 설정    --------------------------------------------------
+st.set_page_config(page_title="보장분석 대시보드", layout='wide')
+
+# -------------------------------------------------  인증페이지 삽입  -------------------------------------------------------
+# 인증모듈 기본설정
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+# 로그인창 노출
+name, authentication_status, username = authenticator.login('Login', 'main')
+# 사이드바에 로그아웃 버튼 추가
+authenticator.logout('Logout', 'sidebar')
+# 인증상태 검증
+if authentication_status == None:
+    st.warning('아이디와 패스워드를 입력해주세요')
+if authentication_status == False:
+    st.error('아이디와 패스워드를 확인해주세요')
+if authentication_status:
+    ########################################################################################################################
+    ##################################################     자료 제작     #####################################################
+    ########################################################################################################################
+    hide_st_style()
+    instance = ServiceData() # 인스턴스 생성
+    st.header("보장분석 사용현황 요약")
+    st.markdown("# 제작중")
+
+    # ----------------------------------------    Google Sheet 데이터베이스 호출    --------------------------------------------- 
+    df_month = instance.call_data_service()
+
+    st.markdown('---')
+    st.markdown('### 보장분석 사용현황 통계')
+    st.dataframe(instance.make_set_summary(df_month), use_container_width=True)
+
+    st.markdown('---')
+    st.markdown('### 보장분석 부문별 사용인원')
+    st.dataframe(instance.make_set_branch(df_month), use_container_width=True)
+
+    # 요약보고서 제작
